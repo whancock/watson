49672a7350617e26213ab2b16ed3d71de0b6ec9b
Tory S. Anderson <torys.anderson@gmail.com>
2015-04-22 22:24:52 -0400

    Spacing, comment fixes

--- public/app/controllers.js
+++ public/app/controllers.js
@@ -1,123 +1,123 @@
 angular.module('watson')
 
-.controller('WatsonCtrl', ['WsWatson', 'WsConversations', 'WsData', function(WsWatson, WsConversations, WsData) {
+    .controller('WatsonCtrl', ['WsWatson', 'WsConversations', 'WsData', function(WsWatson, WsConversations, WsData) {
 
-    var ctrl = this;
+	var ctrl = this;
 
-    WsData.query(function(data) {
-        ctrl.mockedData = data;
-    });
+	WsData.query(function(data) {
+            ctrl.mockedData = data;
+	});
 
-    ctrl.askWatson = function(question) {
-       WsWatson.ask({
-          question: question
-      }, function(result) {
-    		    //console.log(result);
-              ctrl.watson_result = {
-                 meta: {
-                    qclasslist: result.question.qclasslist,
-                    focuslist: result.question.focuslist,
-                    latlist: result.question.latlist,
-                    synonymlist: result.question.synonymlist,	
-                },
-                answers: result.question.answers,
-                evidence: result.question.evidencelist
-            };
-        });
-   };
+	ctrl.askWatson = function(question) {
+	    WsWatson.ask({
+		question: question
+	    }, function(result) {
+    		//console.log(result);
+		ctrl.watson_result = {
+                    meta: {
+			qclasslist: result.question.qclasslist,
+			focuslist: result.question.focuslist,
+			latlist: result.question.latlist,
+			synonymlist: result.question.synonymlist,	
+                    },
+                    answers: result.question.answers,
+                    evidence: result.question.evidencelist
+		};
+            });
+	};
 
-}])
+    }])
 
-.controller('ProjectCtrl', ['WsData', function(WsData) {
+    .controller('ProjectCtrl', ['WsData', function(WsData) {
 
-   var ctrl = this;
-
-
-   WsData.query(function(data) {
-        //ctrl.mockedData = data;
-        ctrl.setQAPairs = data.seedResponses;
-        ctrl.cannedResponses = data.cannedResponses;
-    });
+	var ctrl = this;
 
 
-   ctrl.savedAnswers = [];
-   ctrl.pinAnswer = function(question, answer) {
-    ctrl.savedAnswers.push({
-        question: question,
-        answer: answer
-    });
-};
-
-ctrl.needsApproval = false;
-ctrl.currentQuestion = '';
-ctrl.currentAnswer = '';
+	WsData.query(function(data) {
+            //ctrl.mockedData = data;
+            ctrl.setQAPairs = data.seedResponses;
+            ctrl.cannedResponses = data.cannedResponses;
+	});
 
 
+	ctrl.savedAnswers = [];
+	ctrl.pinAnswer = function(question, answer) {
+	    ctrl.savedAnswers.push({
+		question: question,
+		answer: answer
+	    });
+	};
 
-	   // Questions for the chat: instantly approved. Merges .askQuestion() and .approve()
-	   ctrl.chatQuestion = function(question) {
-
-       ctrl.needsApproval = false;
-       ctrl.currentQuestion = '';
-
-       var cannedAnswer = {
-        evidence: [],
-        link: "NA",
-        answer: "Hmm... I'm not sure."
-    };
-
-    if (question in ctrl.cannedResponses) {
-        cannedAnswer = ctrl.cannedResponses[question].pop();
-    }
-
-    ctrl.currentAnswer = cannedAnswer;
+	ctrl.needsApproval = false;
+	ctrl.currentQuestion = '';
+	ctrl.currentAnswer = '';
 
 
-    this.setQAPairs.push({
-      question: question,
-      answer: cannedAnswer.answer
-  });
 
-    ctrl.question = '';
-};
+	// Questions for the chat: instantly approved. Merges .askQuestion() and .approve()
+	ctrl.chatQuestion = function(question) {
+
+	    ctrl.needsApproval = false;
+	    ctrl.currentQuestion = '';
+
+	    var cannedAnswer = {
+		evidence: [],
+		link: "NA",
+		answer: "Hmm... I'm not sure."
+	    };
+
+	    if (question in ctrl.cannedResponses) {
+		cannedAnswer = ctrl.cannedResponses[question].pop();
+	    }
+
+	    ctrl.currentAnswer = cannedAnswer;
+
+
+	    this.setQAPairs.push({
+		question: question,
+		answer: cannedAnswer.answer
+	    });
+
+	    ctrl.question = ''; // clear input box after submission
+	};
 
 
     	/*ctrl.approve = function() {
 
-    	    this.setQAPairs.push({
-        		question: ctrl.currentQuestion,
-        		answer: ctrl.currentAnswer.answer
-    	    });
+    	  this.setQAPairs.push({
+          question: ctrl.currentQuestion,
+          answer: ctrl.currentAnswer.answer
+    	  });
 
-    	    ctrl.needsApproval = false;
-    	    ctrl.currentQuestion = '';
-    	    ctrl.currentAnswer = {};
+    	  ctrl.needsApproval = false;
+    	  ctrl.currentQuestion = '';
+    	  ctrl.currentAnswer = {};
 
-    	};
+    	  };
 
-    	ctrl.deny = function() {
-    	    ctrl.currentAnswer = cannedResponses[ctrl.currentQuestion].pop();
-    	};*/
+    	  ctrl.deny = function() {
+    	  ctrl.currentAnswer = cannedResponses[ctrl.currentQuestion].pop();
+    	  };*/
 
 
     }])
 
 
-  .controller('ForumControl', function(WsForum) {
+    .controller('ForumControl', function(WsForum) {
 
 
-    var ctrl = this;
+	var ctrl = this;
 
-    WsForum.query(function(data) {
+	WsForum.query(function(data) {
 
-      ctrl.fdata = data;
+	    ctrl.fdata = data;
 
-    });
+	});
 
-    ctrl.showDetails = function(question) {
-      ctrl.detailQuestion = question;
-    }
+	ctrl.showDetails = function(question) {
+	    ctrl.detailQuestion = question;
+	}
 
 
-  })
+    })
 ;
